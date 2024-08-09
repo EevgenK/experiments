13fdde2f37f1d7fbc191fd24146dfee185e6c10e
Evhen Kulbachenko <45nalitay45@gmail.com>
2024-08-10 00:48:21 +0300

    check

---
+++ src/js/cats.js
@@ -0,0 +1,98 @@
+import SlimSelect from 'slim-select';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import { fetchBreeds, fetchCatByBreed } from './helpers/getApi';
+import 'slim-select/dist/slimselect.css';
+const refs = {
+  petSelect: document.querySelector('#pet-select'),
+  select: document.querySelector('#breed-select'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+  info: document.querySelector('.cat-info'),
+};
+
+new SlimSelect({
+  select: '#pet-select',
+  settings: {
+    placeholderText: 'Chose your pet',
+  },
+});
+
+let colection;
+let selectBreed = new SlimSelect({
+  select: '#breed-select',
+});
+const selectMarkup = arr => {
+  let markup = arr
+    .map(a => `<option class='select_color' value="${a.id}">${a.name}</option>`)
+    .join('');
+  const placeholder = '<option data-placeholder="true"></option>';
+  refs.select.innerHTML = '';
+  selectBreed.destroy();
+  refs.select.innerHTML = markup;
+  refs.select.insertAdjacentHTML('afterbegin', placeholder);
+};
+
+const infoMarkup = (imgObj, fullObg) => {
+  return (refs.info.innerHTML = `
+    <img class='info-img' src='${imgObj.url}' />
+  <div class="description">
+      <h2>${fullObg.name}</h2>
+      <p>${fullObg.description}</p>
+      <p><span class='temperamrnt'>Temperament: </span>${fullObg.temperament}</p>
+    </div>
+    `);
+};
+const toHideElement = el => el.classList.add('hidden');
+const toShowElement = el => el.classList.remove('hidden');
+
+const onHandleSelect = e => {
+  toHideElement(refs.petSelect);
+  toHideElement(refs.select);
+  toHideElement(refs.info);
+  toShowElement(refs.loader);
+
+  const targetID = e.currentTarget.value;
+
+  const card = [];
+  colection
+    .then(resp => {
+      return resp.find(el => el.id === Number(targetID) || el.id === targetID);
+    })
+    .then(resp => {
+      card.push(resp);
+    });
+
+  fetchCatByBreed(targetID)
+    .then(resp => {
+      console.log(resp);
+      infoMarkup(...resp, ...card);
+      toShowElement(refs.petSelect);
+      toShowElement(refs.select);
+      toShowElement(refs.info);
+    })
+    .catch(err =>
+      Notify.failure(' Oops! Something went wrong! Try reloading the page!')
+    )
+    .finally(() => {
+      toHideElement(refs.loader);
+    });
+};
+const onPetSelect = e => {
+  colection = fetchBreeds(e.currentTarget.value);
+  toHideElement(refs.info);
+  colection.then(resp => {
+    selectMarkup(resp);
+    selectBreed = new SlimSelect({
+      select: '#breed-select',
+      settings: {
+        placeholderText: 'Chose your pet',
+      },
+    });
+    toShowElement(refs.select);
+  });
+};
+toHideElement(refs.select);
+
+refs.petSelect.addEventListener('change', onPetSelect);
+
+refs.select.addEventListener('change', onHandleSelect);
--- src/js/helpers/getApi.js
+++ src/js/helpers/getApi.js
@@ -1,3 +1,21 @@
+import axios from 'axios';
+
+axios.defaults.headers.common['x-api-key'] =
+  'live_UyX6Lvoafpw1TLCC7wZpZ8IP5h7v1HI7WbEG0bh7lTeSZzWmJ5TFmvpTcvvLyKyE';
+let AXIOUS_URL = 'https://api.thecatapi.com/v1';
+
+function fetchBreeds(pet) {
+  AXIOUS_URL = `https://api.the${pet}api.com/v1`;
+  return fetch(`${AXIOUS_URL}/breeds?breed_ids=beng&x-api-key`).then(resp =>
+    resp.json()
+  );
+}
+
+function fetchCatByBreed(breedId) {
+  return fetch(
+    `${AXIOUS_URL}/images/search?breed_ids=${breedId}&x-api-key`
+  ).then(resp => resp.json());
+}
 function getWeather(days, city) {
   const baseUrl = 'https://api.weatherapi.com/v1';
   const apiKey = 'c9adebbe6f8f4e05956132625240408&q';
@@ -25,4 +43,4 @@ function getNewsApi(value) {
     .catch(response => console.log(response.message));
 }
 
-export { getWeather, getNewsApi };
+export { getWeather, getNewsApi, fetchBreeds, fetchCatByBreed };
