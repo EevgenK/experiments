8737ca4640925d336450dad37c96ec3bc8b3938f
Evgen Kulbachenko <45nalitay45@gmail.com>
2025-02-20 22:07:26 +0200

    check

--- src/js/timer.js
+++ src/js/timer.js
@@ -6,3 +6,13 @@ document.querySelector('body').insertAdjacentHTML('afterbegin', currentDate());
 const timer1 = new Timer({ selector: '#timer-1', value: 10 });
 const timer2 = new Timer({ selector: '#timer-2', value: 20 });
 const timer3 = new Timer({ selector: '#timer-3' });
+
+function hello(name) {
+  let phrase = `Hello, ${name}!`;
+
+  say(phrase);
+}
+
+function say(phrase) {
+  alert(`** ${phrase} **`);
+}
